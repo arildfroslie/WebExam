8e0e3d89aa352a7f53d1cb3a6ef9d3c3da2d188e
Tommy <Tommy.ba95@gmail.com>
2022-11-15 17:39:17 +0100

    Now with img

---
+++ electric-games-api/Migrations/20221115163308_updateCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace electric_games_api.Migrations
+{
+    public partial class updateCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Developer",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Games");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Developer",
+                table: "Games",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Games",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReleaseDate",
+                table: "Games",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- electric-games/src/Components/Game/GameItem.tsx
+++ electric-games/src/Components/Game/GameItem.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import IGame from "../../interfaces/IGame";
+import "./Game.css"
 
 const GameItem: FC<IGame> = ({id, name, image, platform, genre, rating }) => {
     return (
         <article className="game-item">
             <h3>{name}</h3>
-            <img src={`https://localhost:7119/images/Game/${image}`} alt={name} />
+            <img className="game-image" src={`https://localhost:7119/images/Game/${image}`} alt={name} />
             <p>Platform: {platform}</p>
             <p>Genre: {genre}</p>
             <p>Rating: {rating}%</p>
